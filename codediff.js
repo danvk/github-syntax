e52e18b91c94a7a91eee7bfeec194042e5bf7d29
Dan Vanderkam <danvdk@gmail.com>
2014-09-22 10:05:52 -0400

    Update codediff.js to get word diffs

--- codediff.js
+++ codediff.js
@@ -259,7 +259,7 @@ differ.prototype.buildView_ = function() {
             'afterStartIndex': afterIdx,
             'jumpLength': jump,
           }).attr('line-no', 1 + afterIdx);
-          
+
           els.push($('<div class=line-no>&hellip;</div>').attr('line-no', 1+beforeIdx).get(0));
           els.push($('<div class="skip code">...</div>').attr('line-no', 1+beforeIdx).get(0));
           els.push($('<div class=line-no>&hellip;</div>').attr('line-no', 1+afterIdx).get(0));
@@ -365,9 +365,9 @@ function html_substr(html, start, count) {
   var div = document.createElement('div');
   div.innerHTML = html;
   var consumed = 0;
-  
+
   walk(div, track);
-  
+
   function track(el) {
     if (count > 0) {
       var len = el.data.length;
@@ -388,7 +388,7 @@ function html_substr(html, start, count) {
       el.data = '';
     }
   }
-  
+
   function walk(el, fn) {
     var node = el.firstChild, oldNode;
     var elsToRemove = [];
@@ -421,42 +421,98 @@ function html_substr(html, start, count) {
   return div.innerHTML;
 }
 
+/**
+ * @param {string} line The line to be split
+ * @return {Array.<string>} Component words in the line. An invariant is that
+ *     splitIntoWords_(line).join('') == line.
+ */
+differ.splitIntoWords_ = function(line) {
+  var LC = 0, UC = 2, NUM = 3, WS = 4, SYM = 5;
+  var charType = function(c) {
+    if (c.match(/[a-z]/)) return LC;
+    if (c.match(/[A-Z]/)) return UC;
+    if (c.match(/[0-9]/)) return NUM;
+    if (c.match(/\s/)) return WS;
+    return SYM;
+  };
 
-// Add character-by-character diffs to a row (if appropriate).
-differ.addCharacterDiffs_ = function(beforeCell, afterCell) {
-  var beforeText = $(beforeCell).text(),
-      afterText = $(afterCell).text(),
-      beforeHtml = $(beforeCell).html(),
-      afterHtml = $(afterCell).html();
-  var sm = new difflib.SequenceMatcher(beforeText.split(''), afterText.split(''));
+  // TODO: consider putting each whitespace char into its own word.
+  // Single words can be [A-Z][a-z]+, [A-Z]+, [a-z]+, [0-9]+ or \s+.
+  var words = [];
+  var lastType = -1;
+  for (var i = 0; i < line.length; i++) {
+    var c = line.charAt(i);
+    var ct = charType(c);
+    if (ct == lastType && ct != SYM ||
+        ct == LC && lastType == UC && words[words.length - 1].length == 1) {
+      words[words.length - 1] += c;
+    } else {
+      words.push(c);
+    }
+    lastType = ct;
+  }
+  return words;
+};
+
+/**
+ * Compute an intra-line diff.
+ * @param {string} beforeText
+ * @param {string} afterText
+ * @return {?Array.<Array>} [before codes, after codes], where each element is a
+ *     list of ('change type', start idx, stop idx) triples. Returns null if
+ *     character differences are not appropriate for this line pairing.
+ */
+differ.computeCharacterDiffs_ = function(beforeText, afterText) {
+  var beforeWords = differ.splitIntoWords_(beforeText),
+      afterWords = differ.splitIntoWords_(afterText);
+
+  // TODO: precompute two arrays; this does too much work.
+  var wordToIdx = function(isBefore, idx) {
+    var words = isBefore ? beforeWords : afterWords;
+    var charIdx = 0;
+    for (var i = 0; i < idx; i++) {
+      charIdx += words[i].length;
+    }
+    return charIdx;
+  };
+
+  var sm = new difflib.SequenceMatcher(beforeWords, afterWords);
   var opcodes = sm.get_opcodes();
-  var minEqualFrac = 0.5;  // suppress char-by-chardiffs if there's less than this much overlap.
+
+  // Suppress char-by-char diffs if there's less than 50% character overlap.
+  // The one exception is pure whitespace diffs, which should always be shown.
+  var minEqualFrac = 0.5;
   var equalCount = 0, charCount = 0;
+  var beforeDiff = '', afterDiff = '';
   opcodes.forEach(function(opcode) {
     var change = opcode[0];
-    var beforeLen = opcode[2] - opcode[1];
-    var afterLen = opcode[4] - opcode[3];
+    var beforeIdx = wordToIdx(true, opcode[1]);
+    var beforeEnd = wordToIdx(true, opcode[2]);
+    var afterIdx = wordToIdx(false, opcode[3]);
+    var afterEnd = wordToIdx(false, opcode[4]);
+    var beforeLen = beforeEnd - beforeIdx;
+    var afterLen = afterEnd - afterIdx;
     var count = beforeLen + afterLen;
-    if (change == 'equal') equalCount += count;
+    if (change == 'equal') {
+      equalCount += count;
+    } else {
+      beforeDiff += beforeText.substring(beforeIdx, beforeEnd);
+      afterDiff += afterText.substring(afterIdx, afterEnd);
+    }
     charCount += count;
   });
-  if (equalCount < minEqualFrac * charCount) return;
-
-  // Splice in "insert", "delete" and "replace" tags.
-  // This is made more difficult by the presence of syntax highlighting, which
-  // has its own set of tags. The two can co-exists if we're careful to only
-  // wrap complete (balanced) DOM trees.
-  var m = differ.htmlTextMapper.prototype.getHtmlSubstring;
-  var beforeMapper = new differ.htmlTextMapper(beforeText, beforeHtml);
-  var afterMapper = new differ.htmlTextMapper(afterText, afterHtml);
+  if (equalCount < minEqualFrac * charCount &&
+      !(beforeDiff.match(/^\s*$/) && afterDiff.match(/^\s*$/))) {
+    return null;
+  }
 
   var beforeOut = [], afterOut = [];  // (span class, start, end) triples
   opcodes.forEach(function(opcode) {
     var change = opcode[0];
-    var beforeIdx = opcode[1];
-    var beforeEnd = opcode[2];
-    var afterIdx = opcode[3];
-    var afterEnd = opcode[4];
+    var beforeIdx = wordToIdx(true, opcode[1]);
+    var beforeEnd = wordToIdx(true, opcode[2]);
+    var afterIdx = wordToIdx(false, opcode[3]);
+    var afterEnd = wordToIdx(false, opcode[4]);
     if (change == 'equal') {
       beforeOut.push([null, beforeIdx, beforeEnd]);
       afterOut.push([null, afterIdx, afterEnd]);
@@ -473,6 +529,29 @@ differ.addCharacterDiffs_ = function(beforeCell, afterCell) {
   });
   beforeOut = differ.simplifyCodes_(beforeOut);
   afterOut = differ.simplifyCodes_(afterOut);
+
+  return [beforeOut, afterOut];
+};
+
+
+// Add character-by-character diffs to a row (if appropriate).
+differ.addCharacterDiffs_ = function(beforeCell, afterCell) {
+  var beforeText = $(beforeCell).text(),
+      afterText = $(afterCell).text();
+  var codes = differ.computeCharacterDiffs_(beforeText, afterText);
+  if (codes == null) return;
+  beforeOut = codes[0];
+  afterOut = codes[1];
+
+  // Splice in "insert", "delete" and "replace" tags.
+  // This is made more difficult by the presence of syntax highlighting, which
+  // has its own set of tags. The two can co-exists if we're careful to only
+  // wrap complete (balanced) DOM trees.
+  var beforeHtml = $(beforeCell).html(),
+      afterHtml = $(afterCell).html();
+  var m = differ.htmlTextMapper.prototype.getHtmlSubstring;
+  var beforeMapper = new differ.htmlTextMapper(beforeText, beforeHtml);
+  var afterMapper = new differ.htmlTextMapper(afterText, afterHtml);
 
   $(beforeCell).empty().html(differ.codesToHtml_(beforeMapper, beforeOut));
   $(afterCell).empty().html(differ.codesToHtml_(afterMapper, afterOut));
